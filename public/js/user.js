dfa71f6d4009790ba33754be1bd2eaaa8e502e1c
booldook <booldook@gmail.com>
2020-03-11 11:06:25 +0900

    user.js

--- public/js/user.js
+++ public/js/user.js
@@ -4,24 +4,47 @@ var $userpw = $(f.userpw);
 var $userpw2 = $(f.userpw2);
 var $username = $(f.username);
 var $email = $(f.email);
+var useridValid = false;
 
 $userid.blur(function(){
-	$.post("/user/idchk", {userid: $(this).val()}, cb);
-	function cb(res) {
-		if(res.length > 0) {
-			$userid.css({"border-color": "red"});
-			$userid.next().remove();
-			$userid.parent().append('<div class="pt-2 text-danger">* 중복된 아이디 입니다.</div>');
-			$userid.focus();
-		}
-		else {
-			$userid.next().remove();
-			$userid.css({"border-color": "blue"});
+	if($userid.val().trim() == "" || $userid.val().trim().length < 4) {
+		useridFail("아이디는 최소 4자 이상입니다.");
+	}
+	else {
+		$.post("/user/idchk", {userid: $(this).val()}, cb);
+		function cb(res) {
+			if(res.length > 0) {
+				useridFail("중복된 아이디가 있습니다. 다른 아이디를 선택해 주세요.");
+			}
+			else {
+				$userid.next().remove();
+				$userid.css({"border-color": "blue"});
+				useridValide = true;
+			}
 		}
 	}
 });
 
 
+function signupSubmit() {
+	if($userid.val().trim() === "" || $userid.val().trim().length < 4) {
+		useridFail("아이디는 최소 4자 이상입니다.");
+		return false;
+	}
+	if(!useridValid) {
+		alert("아이디를 중복체크 하세요.");
+		$userid.focus();
+		return false;
+	}
+}
+
+function useridFail(msg) {
+	$userid.css({"border-color": "red"});
+	$userid.next().remove();
+	$userid.parent().append('<div class="pt-2 text-danger">* '+msg+'</div>');
+	$userid.focus();
+	useridValide = false;
+}
 
 
 /*
